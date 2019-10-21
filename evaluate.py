00b70c3c39797b677c2766ea966a52d13036aaf3
Dan Wells <dan.wells@ed.ac.uk>
2019-10-21 13:34:30 +0100

    make evaluation not be pandas

--- evaluate.py
+++ evaluate.py
@@ -1,56 +1,86 @@
 #!/usr/bin/env python3
 
-import pandas as pd
+import csv
+from collections import defaultdict
+
+# TODO: is it useful to include indices per sentence anywhere?
 
 def baseline_copy(reference):
     """Evaluate lemmatization accuracy when copying surface form to lemma."""
     ref_header = ["id", "form", "lemma", "upostag", "xpostag",
                   "feats", "head", "deprel", "deps", "misc"]
-    # TODO: add index per sentence (is it useful?)
     # quoting stops special interpretation of " PUNCT
-    ref = pd.read_csv(reference, sep='\t', header=None, names=ref_header, quoting=3)
-    ref['form'] = ref['form'].str.lower()
-    ref = ref[ref['xpostag'] != 'PUNCT']
-    copy_acc = (ref['form'] == ref['lemma']).mean()
+    with open(reference) as inf:
+        ref = csv.DictReader(inf, delimiter='\t', fieldnames=ref_header, quoting=csv.QUOTE_NONE)
+        total = 0
+        correct = 0
+        for word in ref:
+            if word['xpostag'] == 'PUNCT':
+                continue
+            word['form'] = word['form'].lower()
+            if word['form'] == word['lemma']:
+                correct += 1
+            total += 1
+        copy_acc = correct / total
     return copy_acc
 
 def baseline_most_frequent(train, valid):
     """Evaluate lemmatization accuracy predicting most common lemma."""
     ref_header = ["id", "form", "lemma", "upostag", "xpostag",
                   "feats", "head", "deprel", "deps", "misc"]
-    df_train = pd.read_csv(train, sep='\t', header=None, names=ref_header, quoting=3)
-    df_train['form'] = df_train['form'].str.lower()
-    df_train = df_train[df_train['xpostag'] != 'PUNCT']
-    most_frequent_lemmas = df_train[['form', 'lemma']].groupby('form')['lemma'].apply(lambda x:x.value_counts().idxmax())
-
-    df_valid = pd.read_csv(valid, sep='\t', header=None, names=ref_header, quoting=3)
-    df_valid['form'] = df_valid['form'].str.lower()
-    df_valid = df_valid[df_valid['xpostag'] != 'PUNCT']
-    df_valid['plemma'] = df_valid['form'].map(most_frequent_lemmas)
-    # missing vocab ends up NaN: predict with surface form
-    mask = df_valid['plemma'].isnull()
-    df_valid.loc[mask, 'plemma'] = df_valid.loc[mask, 'form']
-
-    most_frequent_acc = (df_valid['lemma'] == df_valid['plemma']).mean() 
+    # get most frequent lemmas per form from training data
+    with open(train) as inf:
+        ref = csv.DictReader(inf, delimiter='\t', fieldnames=ref_header, quoting=csv.QUOTE_NONE)
+        lemma_counts = defaultdict(lambda : defaultdict(int))
+        for word in ref:
+            if word['xpostag'] == 'PUNCT':
+                continue
+            word['form'] = word['form'].lower()
+            lemma_counts[word['form']][word['lemma']] += 1
+        most_frequent_lemmas = {k: max(v.items(), key=lambda x: x[1])[0] for k, v in lemma_counts.items()}
+    # predict over validation data
+    with open(valid) as inf:
+        words = csv.DictReader(inf, delimiter='\t', fieldnames=ref_header, quoting=csv.QUOTE_NONE)
+        total = 0
+        correct = 0
+        for word in words:
+            if word['xpostag'] == 'PUNCT':
+                continue
+            word['form'] = word['form'].lower()
+            try:
+                pred = most_frequent_lemmas[word['form']]
+            # predict surface form for unseen words
+            except KeyError:
+                pred = word['form']
+            if pred == word['lemma']:
+                correct += 1
+            total += 1
+    most_frequent_acc = correct / total
     return most_frequent_acc
 
-def evaluate_predicted(reference, predicted):
+def evaluate_predicted(reference, predicted, merged='merged.tsv'):
     """Evaluate lemmatization accuracy of predictions in one CoNNL file against another."""
     ref_header = ["id", "form", "lemma", "upostag", "xpostag",
                   "feats", "head", "deprel", "deps", "misc"]
-    # TODO: add index per sentence (would be useful for more certain merging)
-    # quoting stops special interpretation of " PUNCT
-    ref = pd.read_csv(reference, sep='\t', header=None, names=ref_header, quoting=3)
-    ref = ref[ref['xpostag'] != 'PUNCT']
-
-    # TODO: sentence indices also would help clear out PUNCT, in case there are
-    # mistakes in predicted output (not sure if marmot ignores PUNCT)
+    # TODO: make this less specific to Lemming output format
     # pX = predicted X
     pred_header = ["id", "form", "lemma", "plemma",
                    "pos", "ppos", "feats", "pfeats"]
-    pred = pd.read_csv(predicted, sep='\t', header=None, names=pred_header, quoting=3)
-    pred = pred[pred['ppos'] != 'PUNCT'] 
-    # fill in gold values
-    pred[['lemma', 'pos', 'feats', 'misc']] = ref[['lemma', 'xpostag', 'feats', 'misc']]
-    pred_acc = (pred['lemma'] == pred['plemma']).mean()
-    return pred, pred_acc
+    merge_header = pred_header + ["misc"]
+    total = 0
+    correct = 0
+    with open(reference) as ref, open(predicted) as pred, open(merged, 'w') as merge:
+        merge_tsv = csv.DictWriter(merge, delimiter='\t', fieldnames=merge_header, quoting=csv.QUOTE_NONE)
+        ref_words = csv.DictReader(ref, delimiter='\t', fieldnames=ref_header, quoting=csv.QUOTE_NONE)
+        pred_words = csv.DictReader(pred, delimiter='\t', fieldnames=pred_header, quoting=csv.QUOTE_NONE)
+        for rw, pw in zip(ref_words, pred_words):
+            if rw['xpostag'] == 'PUNCT':
+                continue
+            if rw['lemma'] == pw['plemma']:
+                correct += 1
+            total += 1
+            merge_word = pw
+            merge_word.update({'lemma': rw['lemma'], 'pos': rw['xpostag'], 'feats': rw['feats'], 'misc': rw['misc']})
+            merge_tsv.writerow(merge_word)
+    pred_acc = correct / total
+    return pred_acc
