1b69589201b794bf695754e9f9ae63308a85b3f4
rimvydasrub <rimvydasrub@gmail.com>
2019-10-25 09:38:36 +0100

    general purpose nmt_model trainer

---
+++ nmt_model/trainer.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+exp_dir=$1
+mode=$2
+
+echo "Starting Experiment $exp_dir" 
+
+python util.py --mode $mode \
+               --src_name $exp_dir/train.src \
+               --tgt_name $exp_dir/train.tgt
+
+python util.py --mode $mode \
+               --src_name $exp_dir/valid.src \
+               --tgt_name $exp_dir/valid.tgt
+
+onmt_preprocess -train_src $exp_dir/train.$2.src  \
+                -train_tgt $exp_dir/train.$2.tgt  \
+                -save_data $exp_dir/exp
+
+onmt_train -data $exp_dir/exp \
+           -save_model $exp_dir/exp_model \
+           -config $exp_dir/train.yaml
+
+
+onmt_translate -model $exp_dir/exp_model\_step_5000.pt \
+               -config $exp_dir/translate.yaml \
+               -src $exp_dir/valid.$2.src \
+               -output $exp_dir/valid.$2.pred 
+
+python util.py --mode eval \
+                --predict_name $exp_dir/valid.$2.pred  \
+                --tgt_name $exp_dir/valid.$2.tgt \
+                --pos_name $exp_dir/valid.pos >> $exp_dir/eval.txt
+
+echo "Finished Experiment $exp_dir"
